c9cfacea7aef5bda9915553de8764811b8c7c88d
D B <86744141+gizmanod@users.noreply.github.com>
2022-03-10 22:49:12 +0200

    Update fix10.sh

--- fix10.sh
+++ fix10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl stop casper-node-launcher
 CASPER_VERSION=1_4_4
-TRUSTED_HASH=804de698297f35607a55473376080cc576e2187401cee78a126dcc7175bede9f
+TRUSTED_HASH=b466bc4c9d73cd44aee1a740cc416bf9d9b25a4a49318384a286830992b77ab0
 if [ "$TRUSTED_HASH" != "null" ]; then sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/$CASPER_VERSION/config.toml; fi
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo systemctl start casper-node-launcher
